bcfd2452ea6734595be2c15b87e8a082caad3cde
Paras Arya <parasarya289@gmail.com>
2024-06-25 05:00:40 +0530

    feat: routes for different pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Dashboard } from "./Pages/Dashboard/Dasboard";
-
+import { BarChartData } from "./Pages/BarChart/BarChart";
+import { PieChartData } from "./Pages/PieChart/PieChart";
+import { Statistics } from "./Pages/Statistics/Statistics";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Dashboard />} />
-        {/* <Route path="/departments" element={<Departments />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/add-product" element={<AddProduct />} /> */}
+        <Route path="/statistics" element={<Statistics />} />
+        <Route path="/bar-chart" element={<BarChartData />} />
+        <Route path="/pie-chart" element={<PieChartData />} />
       </Routes>
     </div>
   );
